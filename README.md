0a6ca530057e524f0dc6e1e178bab473e962bcca
Anik Das <anikdas0811@gmail.com>
2022-06-05 18:33:15 +0530

    feat: working chunk splitting
    Signed-off-by: Anik Das <anikdas0811@gmail.com>

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # Vite POC
 
+- [ ] Code splitting
+- [x] Fast Hot Module Replacement
+- [x] Linting/Typecheck Logs in console
+- [x] Optimized build output ([by default](https://vitejs.dev/guide/features.html#build-optimizations))
+- [ ] Module separation for dynamic imports
+- [x] Chunk splitting & compression (react, react-dom into a separate bundle for now)
+
+
 ## Type Checking Log
 
 ![type checking log in vite](https://i.imgur.com/TmSrKIU.png)
--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,23 @@ import react from "@vitejs/plugin-react";
 import vitePluginEslint from "vite-plugin-eslint";
 import checker from "vite-plugin-checker";
 
+const SPLIT_CHUNK_LIST = ["react", "react-dom"];
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("node_modules")){
+            if (id.includes("react")){
+              return "vendor_react";
+            }
+            return "vendor";
+          }
+        }
+      }
+    }
+  },
   plugins: [
     react(),
     vitePluginEslint({
